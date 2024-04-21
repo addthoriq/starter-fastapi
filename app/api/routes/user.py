a2586a2c68e8f7b11fed1c7568e138380630f2dc
addthoriq <alvarro.thoriq@gmail.com>
2024-04-21 18:11:37 +0800

    validation check username or email is already stored

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -17,6 +17,11 @@ async def get_users(
         limit: int = 100,
 ):
     db_users = user_controller.get_users(db, skip=skip, limit=limit)
+    if db_users is None:
+        raise HTTPException(
+            status_code=200,
+            detail="No users found",
+        )
     return db_users
 
 @router.post('/', tags=["user"], response_model=schemas.User)
@@ -24,10 +29,29 @@ def create_user(
         user: schemas.UserCreate,
         db: Session = Depends(get_db),
 ):
-    # db_user = user_controller.create_user(db, user)
-    # if db_user:
-    #     raise HTTPException(
-    #         status_code=400,
-    #         detail=f"User {db_user.id} already exists",
-    #     )
+    user_email = user_controller.get_user_by_email(db, user.email)
+    user_username = user_controller.get_user_by_username(db, user.username)
+    if user_email:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+    if user_username:
+        raise HTTPException(
+            status_code=400,
+            detail="Username already registered"
+        )
     return user_controller.create_user(db=db, user=user)
+
+@router.get('/{user_id}', tags=["user"], response_model=schemas.User)
+def get_user(
+        user_id: int,
+        db: Session = Depends(get_db),
+):
+    db_user = user_controller.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
+    return db_user
